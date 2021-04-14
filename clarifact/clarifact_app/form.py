ed8ddef984bc0419cd4dbb50fec4a7c915f2aae1
rahul28aggarwal <rahulagg1997@gmail>
2021-04-14 17:53:41 +1000

    UI for PAC

--- clarifact/clarifact_app/form.py
+++ clarifact/clarifact_app/form.py
@@ -7,7 +7,33 @@ class author_form(forms.Form):
 
 
 class fake_news_form(forms.Form):
+    
+    news_title = forms.CharField(label='',
+                                 widget=forms.TextInput(attrs={'placeholder':'Enter the News Title...',
+                                                              'class':'input-form required',
+                                                              'id':'f_name'}),
+                                                              required=True)
     news_text = forms.CharField(label='', 
-                                widget=forms.Textarea(attrs={'placeholder': 'Enter the news body...'}))
+                                widget=forms.Textarea(attrs={'placeholder': 'Enter the News Body...',
+                                                            'class':'input-form required',
+                                                            'id':'con_message'}),
+                                                            required=True)
+    news_author = forms.CharField(label='',
+                                 widget=forms.TextInput(attrs={'placeholder':'Enter the Author Name...',
+                                                              'class':'input-form',
+                                                              'id':'author'}),
+                                                              required=True)
+    news_date = forms.CharField(label='',
+                                 widget=forms.TextInput(attrs={'placeholder':'Enter the Date of News',
+                                                              'class':'input-form',
+                                                              'id':'date'}),
+                                                              required=True)
+
+
+
+                               
+
+
+# <input class="input-form required" type="text" name="f_name" id="f_name" placeholder="News Title" required>
 
                             
